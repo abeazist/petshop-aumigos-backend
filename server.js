676158a8a966c2f7d4ba2154795e14ac39ed8043
fernanda <fer.r.benetti@gmail.com>
2025-12-04 17:08:22 -0300

    erro do cors resolvido (eu acho)

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
-import {app} from "./src/app.js";
+import { app } from "./src/app.js";
 
-app.listen({ port: 3000 }, () => {
-  console.log("Servidor rodando em http://localhost:3000");
-  
-});
+// async function main() {
+//   try {
+//     await app.listen({ port: 3000 });
+//     console.log("Servidor rodando em http://localhost:3000");
+//   } catch (err) {
+//     console.error("Erro ao iniciar o servidor:", err);
+//     process.exit(1);
+//   }
+// }
+// main();
+
+const PORT = process.env.PORT || 3000;
+
+async function main() {
+  try {
+    await app.listen({ port: PORT, host: "0.0.0.0" });
+    console.log(`Servidor rodando na porta ${PORT}`);
+  } catch (err) {
+    console.error("Erro ao iniciar o servidor:", err);
+    process.exit(1);
+  }
+}
+
+main();
